9e1b0be10ba02a219c0e852d49a6b890904032af
Victor Procure <victor.procure@gmail.com>
2022-04-24 09:00:19 -0600

    feat(Samples): Added client side sample page

---
+++ Fluxor.Forms.Samples.Wasm/Models/TestFormEditModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fluxor.Forms.Samples.Wasm.Models
+{
+    public class TestFormEditModel
+    {
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        public string? FavoriteColour { get; set; }
+
+        public Guid ModelId { get; set; } = Guid.NewGuid();
+    }
+}
---
+++ Fluxor.Forms.Samples.Wasm/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fluxor.Forms.Samples.Wasm
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddFluxor(o => o
+                .ScanAssemblies(typeof(Program).Assembly)
+                .AddFluxorForms());
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
